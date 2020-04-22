6f1f58ec433ded56f6e42bfc10273baebd808f6c
ltcmichelli <42404448+ltcmichelli@users.noreply.github.com>
2020-04-22 16:12:04 +0800

    [Michelle] Add Todolist

---
+++ src/component/TodoList.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class TodoList extends Component {
+    render() {
+        return (
+            <div>
+                Todo Element
+            </div>
+        );
+    }
+}
+
+TodoList.propTypes = {
+
+};
+
+export default TodoList;
