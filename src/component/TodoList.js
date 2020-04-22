e8a769a4a9a51e50740c90641c7ac180fc578d19
ltcmichelli <42404448+ltcmichelli@users.noreply.github.com>
2020-04-22 23:27:07 +0800

    [Michelle] modify UI

--- src/component/TodoList.js
+++ src/component/TodoList.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import TodoListApi from '../apis/TodoListApi';
 import TodoItem from './TodoItem';
-import { Row, Button, Input } from 'antd';
+import { Row, Button, Input, Col, Space } from 'antd';
 import { CloseOutlined } from '@ant-design/icons';
+import { Typography } from 'antd';
 
+const { Title } = Typography;
 
 
 class TodoList extends Component {
@@ -33,14 +35,14 @@ class TodoList extends Component {
     }
 
     onMarkComplete(id, status) {
-        let targetItem = this.state.todoList.map(function(item){
-            if (item.id === id){
+        let targetItem = this.state.todoList.map(function (item) {
+            if (item.id === id) {
                 item.status = !status;
             }
             return item;
-        });      
+        });
         this.setState({ todoList: targetItem });
-        
+
     }
 
     handleChange(event) {
@@ -48,19 +50,19 @@ class TodoList extends Component {
     }
 
     onAddItem(event) {
-            let newList = this.state.todoList;
-            let newId = parseInt(newList.slice(-1).map(item => item.id)[0]) + 1;
-            const addItem = {
-                id: newId.toString(),
-                content: this.state.content,
-                status: false
-            }
-            newList.push(addItem);
-            this.setState({ todoList: newList });
+        let newList = this.state.todoList;
+        let newId = parseInt(newList.slice(-1).map(item => item.id)[0]) + 1;
+        const addItem = {
+            id: newId.toString(),
+            content: this.state.content,
+            status: false
+        }
+        newList.push(addItem);
+        this.setState({ todoList: newList });
         event.preventDefault();
     }
 
-    onDeleteItem(event){
+    onDeleteItem(event) {
         let filteredList = this.state.todoList.filter(item => item.id !== event.target.id);
         this.setState({ todoList: filteredList });
     }
@@ -69,16 +71,23 @@ class TodoList extends Component {
         let TodoItemList = this.state.todoList;
         return (
             <div>
-                <Input type="text" placeholder="Input todo item" value={this.state.value} onChange={this.handleChange} />
-                <Button type="submit" onClick={this.onAddItem}>Submit</Button>
+                <Title>Todo List</Title>
+                <Row>
+                    <Col span={24}>
+                        <Input type="text" placeholder="Input todo item" value={this.state.value} onChange={this.handleChange} />
+                        <Button type="primary" onClick={this.onAddItem}>Submit</Button>
+                    </Col>
+                </Row>
+                <br />
                 {TodoItemList.map((todo) => (
                     <div key={todo.id}>
-                        <Row>
-                            <Button id="btnMarkComplete" onClick={() => this.onMarkComplete(todo.id, todo.status)}>
-                                <TodoItem key={todo.id} content={todo.content} status={todo.status}/>
+                        <Space>
+                            <Button style={{ width: '35rem' }} onClick={() => this.onMarkComplete(todo.id, todo.status)}>
+                                <TodoItem key={todo.id} content={todo.content} status={todo.status} />
                             </Button>
-                            <Button type="primary" icon={<CloseOutlined />} size="large" id = {todo.id} onClick={this.onDeleteItem}/>
-                        </Row>
+                            <Button icon={<CloseOutlined />} id={todo.id} onClick={this.onDeleteItem} danger/>
+                        </Space>
+
                     </div>
                 ))}
 
